b6dc28e6c23c18d6f2b46b8cbaffa11d6f4bd8dc
kiyo <18kyohei@gmail.com>
2025-01-05 12:52:37 +0900

    rubocopとhtmlbeautifierを通した。

--- db/migrate/20241229125913_add_not_null_constraint_to_items.rb
+++ db/migrate/20241229125913_add_not_null_constraint_to_items.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 class AddNotNullConstraintToItems < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :items, :name, false
-    change_column_null :items, :price, false
+  def up
+    change_table :items, bulk: true do |t|
+      t.change :name, :string, null: false
+      t.change :price, :integer, null: false
+    end
+  end
+
+  def down
+    change_table :items, bulk: true do |t|
+      t.change :name, :string, null: true
+      t.change :price, :integer, null: true
+    end
   end
 end
