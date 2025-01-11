fe3ef04b4bde85ce6a65afe822c841adce666b35
kiyo <18kyohei@gmail.com>
2025-01-11 21:22:33 +0900

    管理者用の一覧ページの大枠を作成した。

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   resources :tasks
 
   namespace :admin do
-    resources :items, only: [:index, :create, :show, :destory, :update]
+    resources :items, only: [:index, :create, :show, :destory, :update, :new, :edit]
   end
 
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
