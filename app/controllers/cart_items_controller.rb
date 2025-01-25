0dfca001694fc39ef2ddbb27a6b9da2a3844ab50
kiyo <18kyohei@gmail.com>
2025-01-26 08:01:52 +0900

    cart_itemコントローラ内にカート内の増加と削除の機能を追加した

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,12 +1,29 @@
 class CartItemsController < ApplicationController
-  def create
-    increase_or_create.params([:cart_item][:item_id])
-    redierct_to cart_item_path, notice:'商品が追加されました'
+before_action :set_cart, only: %i[increase decrease destroy]
+
+  def index 
+    @cart_items = current_cart.cart_items
   end
 
+  def create
+    increase_or_create.params([:cart_item][:item_id])
+    redierct_to cart_items_path, notice:'商品が追加されました'
+  end
 
+  def increase
+    cart_item.increment!(:quantity, 1)
+  end
+
+  def decrease
+    decrese_or_destroy
+    redierct_to cart_items_path, notice:'商品が削除されました'
+  end
 
   private
+
+  def set_cart
+    @cart_item = current_cart.cart_item.find(params[:id])
+  end
 
   def increase_or_create(item_id)
     cart_item = current_cart.cart_items.find_by(item_id) 
@@ -17,6 +34,12 @@ class CartItemsController < ApplicationController
      end
   end
 
-
+  def decrease_or_destroy(cart_item)
+    if cart_item.quantity > 1
+      cart_item.decrement!(:quantity, 1)
+    else
+      cart_item.destroy
+    end
+  end
 
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,3 @@
 class CartsController < ApplicationController
-  def show
-    @cart = current_cart.cart_items.include(:item)
-  end
+
 end
