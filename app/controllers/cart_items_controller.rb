2d3db079367457faaf7a0d839071ddec193b08d4
kiyo <18kyohei@gmail.com>
2025-02-04 05:55:05 +0900

    詳細ページのカート機能を作成した。

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,7 +2,7 @@ class CartItemsController < ApplicationController
 before_action :set_cart, only: %i[increase decrease destroy]
 
   def index 
-    @cart_items = current_cart.cart_items.include(:item_id)
+  #   @cart_items = current_cart.cart_items.include(:item_id)
   end
 
   def create
@@ -11,13 +11,21 @@ before_action :set_cart, only: %i[increase decrease destroy]
   end
 
   def increase
-    @cart_item.increment!(:quantity, quantity)
-    redirect_to cart_item_path, notice:'カートが更新されました'
+     if @cart_item == nil
+      Rails.logger.debug "Params: #{params.inspect}" 
+      @cart_item = current_cart.cart_items.build(item_id: params[:item_id]).save
+      @cart_item.increment!(:quantity, params.permit(:quantity)[:quantity].to_i)
+      redirect_to item_path, notice:'カートが更新されました'
+     else
+      @cart_item.increment!(:quantity, params.permit(:quantity)[:quantity].to_i)
+      redirect_to item_path(@cart_item.item_id), notice:'カートが更新されました'
+      
+     end
   end
 
 
   def decrease
-    decrease_or_destroy
+    decrease_or_destroys
     redirect_to cart_items_path, notice:'カートが更新されました'
   end
 
@@ -28,16 +36,19 @@ before_action :set_cart, only: %i[increase decrease destroy]
 
   private
 
-  def set_cart
-    
-    @cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
+  def set_cart 
+    #カートの中に商品が入っている場合にセットしておく
+      @cart_item = current_cart.cart_items.find_by(item_id: params.permit(:item_id)[:item_id])
   end
 
   def increase_or_create(item_id)
+    #カートに商品が入っていた時のcart_item
     cart_item = current_cart.cart_items.find_by(item_id: item_id) 
      if cart_item
+      #cart_itemの商品の数が１つ増える。
       cart_item.increment!(:quantity, 1)
      else
+      #初めてカートに商品を入れた時
       current_cart.cart_items.build(item_id: item_id).save 
      end
   end
