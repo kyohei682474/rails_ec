0d3811dc2286823438cc22fa883349389685a700
kiyo <18kyohei@gmail.com>
2025-02-17 05:48:37 +0900

    herokuでエラーにてPGエラーが起こったので改善した

--- db/migrate/20250212090803_remove_same_address_save_info_total_price_from_orders.rb
+++ db/migrate/20250212090803_remove_same_address_save_info_total_price_from_orders.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class RemoveSameAddressSaveInfoTotalPriceFromOrders < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    change_table :orders, bulk: true do |t|
+      t.remove :same_address, :save_info, :total_price
+    end
+  end
+
+  def down
     change_table :orders, bulk: true do |t|
       t.boolean :same_address, default: false, null: false
       t.boolean :save_info, default: false, null: false
+      t.integer :total_price
     end
   end
 end
